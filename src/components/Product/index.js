e59982242f44dd3ecb4739de2863604ba3fbc45a
helloasir <helloasir@yahoo.com>
2022-07-28 00:18:05 +0400

    noon json 1 product added

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,22 +1,23 @@
 import "./index.css";
 
 export default function Product(props) {
-    const {id, imageUrl, name, description, price} = props
+    const {Noon_SKU, Product_Name, Brand_Name, Sold_By} = props
 
     return (
         <div
-            key={id}
+            key={Noon_SKU}
             className={"product"}
         >
-            <img
+            {/* <img
                 src={imageUrl}
                 alt={`Image of ${name}`}
                 className={"image-product"}
-            />
-            <h3>{name}</h3>
-            <p>{description}</p>
-            <span>${price}</span>
-            <div>
+            /> */}
+            <h3>{Noon_SKU}</h3>
+            <p>{Product_Name}</p>
+            <h4> {Brand_Name}</h4>
+            <span>Sold By {Sold_By}</span>
+            {/* <div>
                 <button
                     className="snipcart-add-item"
                     data-item-id={id}
@@ -27,7 +28,7 @@ export default function Product(props) {
                 >
                     Add to Cart
                 </button>
-            </div>
+            </div> */}
         </div>
     );
 }
